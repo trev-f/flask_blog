c090d50d7081279799c8eaebf735da58b1a6b724
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-10-13 11:18:56 -0400

    Add password hashing and user creation at registratino

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
@@ -8,5 +10,7 @@ app.config["SECRET_KEY"] = "94935f10ef555cda09d076573ea6cabc"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import flash, redirect, render_template, url_for
-from flaskblog import app
+from flaskblog import app, bcrypt, db
 from flaskblog.forms import LoginForm, RegistrationForm
 from flaskblog.models import Post, User
 
@@ -34,8 +34,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
-        return redirect(url_for("home"))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your account has been created! You are now able to log in.", "success")
+        return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
 
